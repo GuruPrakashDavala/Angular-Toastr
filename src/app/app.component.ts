07a907258512ca14284fd186e89cec348faf01bd
Guru Prakash <guru.govindaraj@gmail.com>
2020-04-13 12:45:50 +0530

    Check Repository

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(private toastr: ToastrService) {}
+  title = 'checkToast';
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+}
